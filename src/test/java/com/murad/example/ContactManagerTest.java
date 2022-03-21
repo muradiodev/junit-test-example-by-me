331e9f1e296a05cf18f5705b48716976a7095f28
Murad <muradace@gmail.com>
2022-03-21 13:07:42 +0100

    done :)

--- src/test/java/com/murad/example/ContactManagerTest.java
+++ src/test/java/com/murad/example/ContactManagerTest.java
@@ -2,7 +2,13 @@ package com.murad.example;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -49,22 +55,17 @@ class ContactManagerTest {
     }
 
 
-    @DisplayName("repeat contact creation test 5 times")
-    @RepeatedTest(value = 5,
-    name = "Repeating contact creation Test")
-    public void shouldTestContactCreationRepeatedly() {
-        contactManager.addContact("John", "Doe", "0123456789");
+    @DisplayName("MethodSource")
+    @ParameterizedTest
+    @MethodSource("phoneNumberList")
+    public void shouldTestPhoneNumberFormatUsingMethodSource(String phoneNumber) {
+        contactManager.addContact("John", "Doe", phoneNumber);
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
     }
 
-    @DisplayName("repeat contact creation test 5 times")
-    @ParameterizedTest
-    @ValueSource(strings = {"01234516789","0123456789","0123456789"})
-    public void shouldTestContactCreationUsingValueSource(String phoneNumber) {
-        contactManager.addContact("John", "Doe", phoneNumber);
-        assertFalse(contactManager.getAllContacts().isEmpty());
-        assertEquals(1, contactManager.getAllContacts().size());
+    private static List<String> phoneNumberList() {
+        return Arrays.asList("0123456789", "0123456789", "0123456789");
     }
 
 
@@ -92,13 +93,55 @@ class ContactManagerTest {
         });
     }
 
-//    @AfterEach
-//    void tearDown() {
-//        System.out.println("Should execute after each test");
-//    }
-//
-//    @AfterAll
-//    void afterAll() {
-//        System.out.println("Should execute after all");
-//    }
+    @Nested
+    class RepeatedNestedTest {
+        @DisplayName("repeat contact creation test 5 times")
+        @RepeatedTest(value = 5,
+                name = "Repeating contact creation Test")
+        public void shouldTestContactCreationRepeatedly() {
+            contactManager.addContact("John", "Doe", "0123456789");
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+    }
+
+    @Nested
+    class ParameterizedNestedTest {
+        @DisplayName("Value source")
+        @ParameterizedTest
+        @ValueSource(strings = {"0123456789", "0123456789", "0123456789"})
+        public void shouldTestContactCreationUsingValueSource(String phoneNumber) {
+            contactManager.addContact("John", "Doe", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @DisplayName("CSV Source case")
+        @ParameterizedTest
+        @CsvSource({"0123456789", "0123456789", "0123456789"})
+        public void shouldTestPhoneNumberFormatUsingCSVSource(String phoneNumber) {
+            contactManager.addContact("John", "Doe", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @DisplayName("CSV File Source case")
+        @ParameterizedTest
+        @CsvFileSource(resources = "/data.csv")
+        public void shouldTestPhoneNumberFormatUsingCSVFileSource(String phoneNumber) {
+            contactManager.addContact("John", "Doe", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Should execute after each test");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println("Should execute after all");
+    }
 }
